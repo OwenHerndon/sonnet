e4843f9d36a9f7f5a50a32390a5ab0814a8bcefd
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-07-26 18:41:48 -0500

    added switch

--- sonnet.js
+++ sonnet.js
@@ -1,11 +1,45 @@
 var sonnetStr = document.getElementById("sonnet").innerHTML;
-//var sonnetDiv = document.getElementById("sonnet");
+var sonnetDiv = document.getElementById("sonnet");
+var startingPosition = sonnetStr.indexOf("orphans");
 
-var orphans = "orphans";
-sonnetStr.indexOf("orphans");
-console.log(sonnetStr.indexOf("orphans"));
+console.log("starting position of orphans",sonnetStr.indexOf("orphans"));
 
-console.log(sonnetStr.length);
+//var orphans = "orphans";
+//sonnetStr.indexOf("orphans");
+//console.log(sonnetStr.indexOf("orphans"));
+
+if (startingPosition === 422) 
+{
+	console.log("right");
+}
+
+switch (startingPosition){
+	
+	case 422 :
+	console.log("right");
+	break;
+	case 421 :
+	console.log("wrong");
+	break;
+	default :
+	console.log("wtf");
+	break;
+}
+
+switch (false){
+	
+	case startingPosition > 410 :
+	console.log("right");
+	break;
+	case startingPosition <= 410 :
+	console.log("wrong");
+	break;
+	default :
+	console.log("wtf");
+	break;
+}
+
+console.log("number of characters",sonnetStr.length);
 
 
 //sonnetStr = sonnetStr.replace(/the/i,"a large").replace(/winter/,"yuletide");
@@ -19,9 +53,14 @@ console.log(sonnetStr.length);
 //sonnetDiv = sonnetStr.replace(/the/i,"a large").replace(/winter/,"yuletide");
 //console.log(sonnetStr);
 
-document.getElementById("sonnet").innerHTML = sonnetStr.replace(/the/i,"a large").replace(/winter/,"yuletide");
-console.log(sonnetStr);
+//document.getElementById("sonnet").innerHTML = sonnetStr.replace(/the /g,"a large").replace(/winter/g,"yuletide");
+//console.log(sonnetStr);
 
+var newContent = sonnetStr.replace("winter","yuletide").replace(/the /g,"a large");
+//console.log("changed winther and the to yuletide & a large",newContent);
+
+sonnetStr.innerHTML = newContent;
+//document.getElementById(sonnet).innerHTML = newContent
 
 //var winter = "winter";
 //var the = "the";
